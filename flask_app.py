8dd92a9852cda4be26232183fc121f4f2a51cf0c
curcoder <jesco1979@hotmail.com>
2019-05-12 20:30:51 -0400

    changes added

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from users.routes import users
+from main.routes import main
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-	return render_template('home.html')
-
-
-@app.route('/about')
-def about():
-	return render_template('about.html')
-
-
+app.register_blueprint(main)
 app.register_blueprint(users)
 
 
